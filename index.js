be186c9b5361dabeebd5c1a6c14dcc80e57d9c65
nathanallen <thenathanator@gmail.com>
2017-05-25 09:15:37 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/* setup steps
+ * 1. git init
+ * 1. touch .gitignore
+ * 1. npm init  # creates package.json
+ * 1. npm install --save express mongoose
+ * 1.
+ */
+
+
+/* server.js */
+
+
+/* files */
+/* files */
+/* files */
+/* files */
+/* files */
