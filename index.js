bc13903169485c1d9bfbcb513a0aafbf2480b39a
nathanallen <thenathanator@gmail.com>
2017-05-26 09:28:57 -0700

    end of d1 live code

--- index.js
+++ index.js
@@ -3,23 +3,50 @@
  * 1. touch .gitignore
  * 1. npm init  # creates package.json
  * 1. npm install --save express # adds express dependency
- * 1.
+ * 1. npm install --save mongoose
  */
 
+/* models/index.js */
+// module.exports.Unicorn = require("./unicorn.js")
+
+/* models/unicorn.js */
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/wow-unicorns-app");
+
+var Schema = mongoose.Schema;
+
+var unicornSchema = new Schema({
+  name: String,
+  sparkles: Boolean,
+  number_of_horns: {
+    type: Number,
+    default: 1
+  }
+});
+
+var Unicorn = mongoose.model("Unicorn", unicornSchema);
+module.exports = Unicorn;
 
 /* index.js --- server.js */
 var express = require("express");
 var app = express();
 
+// var db = require("./models")
+// var Unicorn = db.Unicorn;
+
 // app.use(express.static("public"));
 
 app.get("/api/unicorns", function index(req, res){
-  res.send([]); // all the unicorns
+  Unicorn.find({}, function(err, unicorns){
+    res.send(unicorns); // all the unicorns
+  });
 })
 
 app.get("/api/unicorns/:id", function show(req, res){
-  //var id = req.params.id;
-  res.send({}); // one unicorn
+  var id = req.params.id;
+  Unicorn.findOne({_id: id}, function(err, unicorn){
+    res.send(unicorn); // one unicorn
+  });
 })
 
 app.post("/api/unicorns", function create(req, res){
@@ -38,7 +65,6 @@ app.listen(3000, function(){
   console.log("Server running on port: localhost:3000");
 });
 
-/* files */
 /* files */
 /* files */
 /* files */
